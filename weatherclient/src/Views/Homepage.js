9ceddf883878bfe4cd814a2e8efd0b94d53d6477
Quen2 <ZexFrost1@gmail.com>
2023-10-05 14:16:04 +0200

    [~] Fix unused useEffect

--- weatherclient/src/Views/Homepage.js
+++ weatherclient/src/Views/Homepage.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react"
+import { React, useState } from "react"
 import Weather from "../Components/Weather"
 import axios from "axios";
 import Header from "../Components/Header";
